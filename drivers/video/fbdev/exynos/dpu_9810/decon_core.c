0130bce7f6a273059753d8d62a06a8e6c803489c
Jiun Yu <jiun.yu@samsung.com>
2018-08-08 07:25:01 +0900

    drivers: video: fbdev: exynos: dpu_9810: fix prevent bug(CID:218843)
    Change-Id: I91c3c464c5c41c9a0bcedb81af265f0017e907cd
    Signed-off-by: Jiun Yu <jiun.yu@samsung.com>

--- drivers/video/fbdev/exynos/dpu_9810/decon_core.c
+++ drivers/video/fbdev/exynos/dpu_9810/decon_core.c
@@ -1348,6 +1348,11 @@ static unsigned int decon_map_ion_handle(struct decon_device *decon,
 	dma->fence = NULL;
 	dma->dma_buf = buf;
 
+	if (IS_ERR_OR_NULL(dev)) {
+		decon_err("%s: dev ptr is invalid\n", __func__);
+		goto err_buf_map_attach;
+	}
+
 	dma->attachment = dma_buf_attach(dma->dma_buf, dev);
 	if (IS_ERR_OR_NULL(dma->attachment)) {
 		decon_err("dma_buf_attach() failed: %ld\n",
@@ -1392,7 +1397,7 @@ static int decon_import_buffer(struct decon_device *decon, int idx,
 	struct decon_dma_buf_data *dma_buf_data = NULL;
 	struct displayport_device *displayport;
 	struct dsim_device *dsim;
-	struct device *dev;
+	struct device *dev = NULL;
 	int ret = 0, i;
 	size_t buf_size = 0;
 
@@ -3169,7 +3174,7 @@ static int decon_fb_alloc_memory(struct decon_device *decon, struct decon_win *w
 	struct fb_info *fbi = win->fbinfo;
 	struct displayport_device *displayport;
 	struct dsim_device *dsim;
-	struct device *dev;
+	struct device *dev = NULL;
 	unsigned int real_size, virt_size, size;
 	dma_addr_t map_dma;
 	struct dma_buf *buf;
@@ -3250,7 +3255,7 @@ static int decon_fb_test_alloc_memory(struct decon_device *decon, u32 size)
 	struct decon_win *win = decon->win[decon->dt.dft_win];
 	struct displayport_device *displayport;
 	struct dsim_device *dsim;
-	struct device *dev;
+	struct device *dev = NULL;
 	dma_addr_t map_dma;
 	struct dma_buf *buf;
 	void *vaddr;
