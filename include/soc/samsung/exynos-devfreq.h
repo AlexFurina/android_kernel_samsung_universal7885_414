cf0ed0d6c7d043fe948a429292c35d1bf24fb7be
AlexFurina <92114905+AlexFurina@users.noreply.github.com>
2026-04-17 22:28:45 +0900

    devfreq: exynos: Add Exynos7885 support

--- include/soc/samsung/exynos-devfreq.h
+++ include/soc/samsung/exynos-devfreq.h
@@ -25,6 +25,19 @@
 #define SET_CONST			1
 #define RELEASE				2
 
+#ifdef CONFIG_ARM_EXYNOS7885_BUS_DEVFREQ
+enum exynos_devfreq_type {
+	DEVFREQ_MIF = 0,
+	DEVFREQ_INT,
+	DEVFREQ_DISP,
+	DEVFREQ_CAM,
+	DEVFREQ_INTCAM,
+	DEVFREQ_AUD,
+	DEVFREQ_FSYS,
+	DEVFREQ_TYPE_END
+};
+#endif
+
 /* DEVFREQ GOV TYPE */
 #define SIMPLE_INTERACTIVE 0
 
@@ -33,6 +46,75 @@ struct exynos_devfreq_opp_table {
 	u32 freq;
 	u32 volt;
 };
+
+#ifdef CONFIG_ARM_EXYNOS7885_BUS_DEVFREQ
+struct um_exynos;
+
+struct exynos_devfreq_ops {
+	/* ops.init(struct exynos_devfreq_data *data) */
+	int (*init)(struct exynos_devfreq_data *);
+	/* ops.exit(struct exynos_devfreq_data *data) */
+	int (*exit)(struct exynos_devfreq_data *);
+	/* ops.init_freq_table(struct exynos_devfreq_data *data) */
+	int (*init_freq_table)(struct exynos_devfreq_data *);
+	/* ops.get_volt_table(struct device *dev, u32 max_state, struct exynos_devfreq_opp_table *opp_list) */
+	int (*get_volt_table)(struct device *, u32, struct exynos_devfreq_opp_table *);
+	/* ops.ppmu_register(struct exynos_devfreq_data *data) */
+	int (*um_register)(struct exynos_devfreq_data *);
+	/* ops.ppmu_unregister(struct exynos_devfreq_data *data) */
+	int (*um_unregister)(struct exynos_devfreq_data *);
+	/* ops.suspend(struct exynos_devfreq_data *data) */
+	int (*suspend)(struct exynos_devfreq_data *);
+	/* ops.resume(struct exynos_devfreq_data *data */
+	int (*resume)(struct exynos_devfreq_data *);
+	/* ops.reboot(struct exynos_devfreq_data *data */
+	int (*reboot)(struct exynos_devfreq_data *);
+	/* ops.get_switch_voltage(struct device *dev, u32 cur_freq, u32 new_freq, u32 cur_volt, u32 new_volt, u32 *switch_volt) */
+	int (*get_switch_voltage)(struct device *, u32, u32, u32, u32, u32 *);
+	/* ops.set_voltage_prepare(struct exynos_devfreq_data *data) */
+	void (*set_voltage_prepare)(struct exynos_devfreq_data *);
+	/* ops.set_voltage_post(struct exynos_devfreq_data *data) */
+	void (*set_voltage_post)(struct exynos_devfreq_data *);
+	/* ops.get_switch_freq(struct device *dev, u32 cur_freq, u32 new_freq, u32 *switch_freq) */
+	int (*get_switch_freq)(struct device *, u32, u32, u32 *);
+	/* ops.get_freq(struct device *dev, u32 *cur_freq, struct clk *clk) */
+	int (*get_freq)(struct device *, u32 *, struct clk *, struct exynos_devfreq_data *);
+	/* ops.set_freq(struct device *dev, u32 new_freq, struct clk *clk) */
+	int (*set_freq)(struct device *, u32, struct clk *, struct exynos_devfreq_data *);
+	/* ops.set_freq_prepare(struct exynos_devfreq_data *data) */
+	int (*set_freq_prepare)(struct exynos_devfreq_data *);
+	/* ops.set_freq_post(struct exynos_devfreq_data) */
+	int (*set_freq_post)(struct exynos_devfreq_data *);
+	/* ops.change_to_switch_freq(struct device *dev, void *private_data, struct clk *sw_clk, u32 switch_freq, u32 cur_freq, u32 new_freq) */
+	int (*change_to_switch_freq)(struct device *, void *, struct clk *, u32, u32, u32);
+	/* ops.restore_from_switch_freq(struct device *dev, void *priave_data, struct clk *clk, u32 cur_freq, u32 new_freq) */
+	int (*restore_from_switch_freq)(struct device *, void *, struct clk *, u32, u32);
+	/* ops.get_dev_status(struct exynos_devfreq_data *data) */
+	int (*get_dev_status)(struct exynos_devfreq_data *);
+	/* ops.cl_dvfs_start(struct device *dev) */
+	int (*cl_dvfs_start)(struct device *);
+	/* ops.cl_dvfs_stop(struct device *dev, u32 target_idx) */
+	int (*cl_dvfs_stop)(struct device *, u32);
+	/* ops.cmu_dump(struct exynos_devfreq_data *data) */
+	int (*cmu_dump)(struct exynos_devfreq_data *);
+	/* ops.pm_suspend_prepare(struct exynos_devfreq_data *data) */
+	int (*pm_suspend_prepare)(struct exynos_devfreq_data *);
+	/* ops.pm_post_suspend(struct exynos_devfreq_data *data) */
+	int (*pm_post_suspend)(struct exynos_devfreq_data *);
+};
+
+struct um_exynos {
+	struct list_head node;
+	void __iomem **va_base;
+	u32 *pa_base;
+	u32 *mask_v;
+	u32 *mask_a;
+	u32 *channel;
+	unsigned int um_count;
+	u64 val_ccnt;
+	u64 val_pmcnt;
+};
+#endif
 
 struct exynos_devfreq_data {
 	struct device				*dev;
@@ -95,6 +177,9 @@ struct exynos_devfreq_data {
 	struct exynos_dm_constraint		**constraint;
 #endif
 	void					*private_data;
+#ifdef CONFIG_ARM_EXYNOS7885_BUS_DEVFREQ
+	struct exynos_devfreq_ops		ops;
+#endif
 	bool					use_acpm;
 	bool					bts_update;
 	bool					update_fvp;
