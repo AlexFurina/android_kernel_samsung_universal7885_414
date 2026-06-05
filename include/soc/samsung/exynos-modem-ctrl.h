e44ecaf81b491d80c449233e89729ad1278514ab
AlexFurina <92114905+AlexFurina@users.noreply.github.com>
2026-06-05 23:27:03 +0900

    drivers: misc: modem_v1: modem_ctrl_ss310ap: FixUP build error ../drivers/misc/modem_v1/modem_ctrl_ss310ap.c: In function 'send_uart_noti_to_modem': ../drivers/misc/modem_v1/modem_ctrl_ss310ap.c:612:14: error: 'MODEM_CTRL_UART_CP' undeclared (first use in this function)    if (val == MODEM_CTRL_UART_CP) {               ^ ../drivers/misc/modem_v1/modem_ctrl_ss310ap.c:612:14: note: each undeclared identifier is reported only once for each function it appears in ../drivers/misc/modem_v1/modem_ctrl_ss310ap.c:614:22: error: 'EXYNOS_PMU_UART_IO_SHARE_CTRL' undeclared (first use in this function)     exynos_pmu_update(EXYNOS_PMU_UART_IO_SHARE_CTRL, 0x3 << 24, 1 << 24); /* CP UART TXD */                       ^ make[4]: *** [../scripts/Makefile.build:356: drivers/misc/modem_v1/modem_ctrl_ss310ap.o] Error 1 make[3]: *** [../scripts/Makefile.build:671: drivers/misc/modem_v1] Error 2 make[3]: *** Waiting for unfinished jobs....

--- include/soc/samsung/exynos-modem-ctrl.h
+++ include/soc/samsung/exynos-modem-ctrl.h
@@ -13,12 +13,18 @@
 #ifndef __EXYNOS_MODEM_CTRL_H
 #define __EXYNOS_MODEM_CTRL_H
 
+#define MODEM_CTRL_UART_AP 0
+#define MODEM_CTRL_UART_CP 1
+
 #if defined(CONFIG_SEC_SIPC_MODEM_IF) || defined(CONFIG_SEC_MODEM_IF)
 extern int modem_force_crash_exit_ext(void);
 extern int modem_send_panic_noti_ext(void);
+extern int ss310ap_send_panic_noti_ext(void);
+extern void send_uart_noti_to_modem(int val);
 #else /* CONFIG_SEC_MODEM_IF */
 static inline int modem_force_crash_exit_ext(void) { return 0; }
 static inline int modem_send_panic_noti_ext(void) { return 0; }
+extern void send_uart_noti_to_modem(int val) {}
 #endif /* CONFIG_SEC_MODEM_IF */
 
 #endif
