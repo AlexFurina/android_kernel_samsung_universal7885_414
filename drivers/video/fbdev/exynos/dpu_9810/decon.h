e18122fe387c8c27d0958fdf24e921de2eb1c2f6
Cho KyongHo <pullip.cho@samsung.com>
2026-08-18 05:52:53 +0900

    drivers: video: fbdev: exynos: dpu_9810: sw_sync is replaced to dma_fence in DPU driver DPU driver doesn't use sw_sync anymore because it is unstable. So, dma_fence is newly used for buffer synchronization in DPU driver.
    Change-Id: I6cd8343fc57c8963c904734b938b7fa5eb95320f
    Signed-off-by: Cho KyongHo <pullip.cho@samsung.com>
    Signed-off-by: Jiun Yu <jiun.yu@samsung.com>

--- drivers/video/fbdev/exynos/dpu_9810/decon.h
+++ drivers/video/fbdev/exynos/dpu_9810/decon.h
@@ -33,6 +33,7 @@
 #ifdef CONFIG_SEC_ABC
 #include <linux/sti/abc_common.h>
 #endif
+#include <linux/sync_file.h>
 
 #ifdef CONFIG_EXYNOS_COMMON_PANEL
 #include "../panel/panel_drv.h"
@@ -43,7 +44,6 @@
 #include "./panels/decon_lcd.h"
 #include "dsim.h"
 #include "displayport.h"
-#include "../../../../dma-buf/sync_debug.h"
 #include "hdr_metadata.h"
 #if defined(CONFIG_EXYNOS_COMMON_PANEL)
 #include "disp_err.h"
@@ -520,7 +520,7 @@ struct decon_dma_buf_data {
 	struct dma_buf_attachment	*attachment;
 	struct sg_table			*sg_table;
 	dma_addr_t			dma_addr;
-	struct sync_file		*fence;
+	struct dma_fence		*fence;
 };
 
 struct decon_win_rect {
@@ -609,6 +609,7 @@ struct decon_reg_data {
 	struct decon_win_rect block_rect[MAX_DECON_WIN];
 	struct decon_window_regs win_regs[MAX_DECON_WIN];
 	struct decon_dma_buf_data dma_buf_data[MAX_DECON_WIN + 1][MAX_PLANE_CNT];
+	struct dma_fence *retire_fence;
 
 	/*
 	 * If window update size is changed, that size has to be applied to
@@ -1080,6 +1081,13 @@ struct decon_systrace_data {
 	pid_t pid;
 };
 
+struct decon_fence {
+	char name[8];
+	u64 context;
+	atomic_t timeline;
+	spinlock_t lock;
+};
+
 struct decon_device {
 	int id;
 	enum decon_state state;
@@ -1091,9 +1099,6 @@ struct decon_device {
 	struct mutex lock;
 	struct mutex pm_lock;
 	spinlock_t slock;
-
-	struct sync_timeline *timeline;
-	int timeline_max;
 
 	struct v4l2_subdev *out_sd[MAX_DSIM_CNT];
 	struct v4l2_subdev *dsim_sd[MAX_DSIM_CNT];
@@ -1121,6 +1126,7 @@ struct decon_device {
 	struct decon_hiber hiber;
 	struct decon_bts bts;
 	struct decon_cursor cursor;
+	struct decon_fence fence;
 
 	int frame_cnt;
 	int frame_cnt_target;
@@ -1595,8 +1601,8 @@ void decon_create_release_fences(struct decon_device *decon,
 		struct sync_file *sync_file);
 #endif
 int decon_create_fence(struct decon_device *decon, struct sync_file **sync_file);
-void decon_wait_fence(struct sync_file *fence);
-void decon_signal_fence(struct decon_device *decon);
+void decon_wait_fence(struct dma_fence *fence);
+void decon_signal_fence(struct dma_fence *fence);
 
 bool decon_intersect(struct decon_rect *r1, struct decon_rect *r2);
 int decon_intersection(struct decon_rect *r1,
