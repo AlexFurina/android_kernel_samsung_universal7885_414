19f1975a3f7c833d548f3e443b701b195721be00
Cho KyongHo <pullip.cho@samsung.com>
2026-08-18 05:52:53 +0900

    drivers: video: fbdev: exynos: dpu_9810: sw_sync is replaced to dma_fence in DPU driver DPU driver doesn't use sw_sync anymore because it is unstable. So, dma_fence is newly used for buffer synchronization in DPU driver.
    Change-Id: I6cd8343fc57c8963c904734b938b7fa5eb95320f
    Signed-off-by: Cho KyongHo <pullip.cho@samsung.com>
    Signed-off-by: Jiun Yu <jiun.yu@samsung.com>

--- drivers/video/fbdev/exynos/dpu_9810/decon_core.c
+++ drivers/video/fbdev/exynos/dpu_9810/decon_core.c
@@ -130,9 +130,8 @@ static void decon_up_list_saved(void)
 		if (decon) {
 			if (!list_empty(&decon->up.list) || !list_empty(&decon->up.saved_list)) {
 				decon->up_list_saved = true;
-				decon_info("\n=== DECON%d TIMELINE %d MAX %d ===\n",
-						decon->id, decon->timeline->value,
-						decon->timeline_max);
+				decon_info("\n=== DECON%d TIMELINE %d ===\n",
+						decon->id, atomic_read(&decon->fence.timeline));
 			} else {
 				decon->up_list_saved = false;
 			}
@@ -408,8 +407,10 @@ static void decon_free_dma_buf(struct decon_device *decon,
 	if (!dma->dma_addr)
 		return;
 
-	if (dma->fence)
-		fput(dma->fence->file);
+	if (dma->fence) {
+		dma_fence_put(dma->fence);
+		dma->fence = NULL;
+	}
 	ion_iovmm_unmap(dma->attachment, dma->dma_addr);
 
 	dma_buf_unmap_attachment(dma->attachment, dma->sg_table,
@@ -1502,7 +1503,7 @@ static int decon_set_win_buffer(struct decon_device *decon,
 {
 	int ret;
 	u32 alpha_length;
-	struct sync_file *fence = NULL;
+	struct dma_fence *fence = NULL;
 	u32 config_size = 0;
 	u32 alloc_size = 0;
 	u32 byte_per_pixel = 4;
@@ -1517,7 +1518,7 @@ static int decon_set_win_buffer(struct decon_device *decon,
 
 	if (config->acq_fence >= 0) {
 		/* fence is managed by buffer not plane */
-		fence = sync_file_fdget(config->acq_fence);
+		fence = sync_file_get_fence(config->acq_fence);
 		regs->dma_buf_data[idx][0].fence = fence;
 		if (!fence) {
 			decon_err("failed to import fence fd\n");
@@ -2196,8 +2197,8 @@ end:
 	DPU_EVENT_LOG(DPU_EVT_TRIG_MASK, &decon->sd, ktime_set(0, 0));
 
 	decon_release_old_bufs(decon, regs, old_dma_bufs, old_plane_cnt);
-	/* signal to acquire fence */
-	decon_signal_fence(decon);
+	decon_signal_fence(regs->retire_fence);
+
 	decon_systrace(decon, 'E', "decon_update_regs", 0);
 
 	DPU_EVENT_LOG(DPU_EVT_FENCE_RELEASE, &decon->sd, ktime_set(0, 0));
@@ -2518,7 +2519,7 @@ static int decon_set_win_config(struct decon_device *decon,
 		if (win_data->retire_fence < 0)
 			goto err;
 		fd_install(win_data->retire_fence, sync_file->file);
-		decon_signal_fence(decon);
+		decon_signal_fence(sync_file->fence);
 		goto err;
 	}
 
@@ -2541,7 +2542,6 @@ static int decon_set_win_config(struct decon_device *decon,
 		if (win_data->retire_fence < 0)
 			goto err_prepare;
 	} else {
-		decon->timeline_max++;
 		win_data->retire_fence = -1;
 	}
 
@@ -2563,6 +2563,7 @@ static int decon_set_win_config(struct decon_device *decon,
 #if defined(CONFIG_DPU_2_0_RELEASE_FENCES)
 		decon_create_release_fences(decon, win_data, sync_file);
 #endif
+		regs->retire_fence = sync_file->fence;
 	}
 
 	decon_hiber_block(decon);
@@ -2601,7 +2602,7 @@ err_prepare:
 	if (win_data->retire_fence >= 0) {
 		/* video mode should keep previous buffer object */
 		if (decon->lcd_info->mode == DECON_MIPI_COMMAND_MODE)
-			decon_signal_fence(decon);
+			decon_signal_fence(sync_file->fence);
 		fput(sync_file->file);
 		put_unused_fd(win_data->retire_fence);
 	}
@@ -2782,17 +2783,7 @@ static int decon_ioctl(struct fb_info *info, unsigned int cmd,
 			break;
 		}
 
-		if ((decon->ver == HWC_INIT) ||
-				(decon->ver != disp_info.ver)) {
-			decon->ver = disp_info.ver;
-			if (decon->ver == HWC_2_0) {
-				decon->timeline_max = 0;
-				decon_info("decon is setting by HWC%d.0\n",
-						decon->ver);
-			} else {
-				decon->timeline_max = 1;
-			}
-		}
+		decon_info("HWC version %d.0 is operating\n", disp_info.ver);
 		disp_info.psr_mode = decon->dt.psr_mode;
 		disp_info.chip_ver = CHIP_VER;
 		disp_info.mres_info = *mres_info;
