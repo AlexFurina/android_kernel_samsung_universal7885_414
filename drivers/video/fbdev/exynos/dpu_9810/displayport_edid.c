f8e6b0a0710bdbbd8a814976094c3dd1b0cd30a1
Dylan Neve <dylanneve1@gmail.com>
2026-08-18 21:19:58 +0900

    drivers: video: fbdev: exynos: dpu_9810: remove SEC displayport logger and bigdata More useless SEC code, SLSI driver doesn't use this and nor does DPU20.

--- drivers/video/fbdev/exynos/dpu_9810/displayport_edid.c
+++ drivers/video/fbdev/exynos/dpu_9810/displayport_edid.c
@@ -868,18 +868,11 @@ int edid_update(struct displayport_device *hdev)
 		goto out;
 	}
 
-#ifdef CONFIG_SEC_DISPLAYPORT_BIGDATA
-	secdp_bigdata_save_item(BD_EDID, edid);
-#endif
-
 	fb_edid_to_monspecs(edid, &specs);
 	modedb_len = specs.modedb_len;
 
 	displayport_info("mon name: %s, gamma: %u.%u\n", specs.monitor,
 			specs.gamma / 100, specs.gamma % 100);
-#ifdef CONFIG_SEC_DISPLAYPORT_BIGDATA
-	secdp_bigdata_save_item(BD_SINK_NAME, specs.monitor);
-#endif
 
 	/* find 2D preset */
 	for (i = 0; i < specs.modedb_len; i++)
