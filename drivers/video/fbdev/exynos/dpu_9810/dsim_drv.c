7564f16fed0ddaa36330e3b76ea68bcfedec7270
AlexFurina <92114905+AlexFurina@users.noreply.github.com>
2026-08-18 09:44:01 +0900

    drivers: video: fbdev: exynos: dpu_9810: Drop CONFIG_SOC_EXYNOS9810 guard * we won't be building this for any other SOC

--- drivers/video/fbdev/exynos/dpu_9810/dsim_drv.c
+++ drivers/video/fbdev/exynos/dpu_9810/dsim_drv.c
@@ -63,7 +63,6 @@ static void __dsim_dump(struct dsim_device *dsim)
 	print_hex_dump(KERN_ERR, "", DUMP_PREFIX_ADDRESS, 32, 4,
 			dsim->res.regs, 0xFC, false);
 
-#if defined(CONFIG_SOC_EXYNOS9810)
 	dsim_info("=== DSIM %d DPHY SFR DUMP ===\n", dsim->id);
 	/* DPHY dump */
 	/* PMSK */
@@ -88,7 +87,6 @@ static void __dsim_dump(struct dsim_device *dsim)
 	/* Data lane : D3 */
 	print_hex_dump(KERN_ERR, "", DUMP_PREFIX_ADDRESS, 32, 4,
 		dsim->res.phy_regs + 0x2080, 0x30, false);
-#endif
 
 	/* restore to avoid size mismatch (possible config error at DECON) */
 	dsim_reg_enable_shadow_read(dsim->id, 1);
@@ -789,10 +787,9 @@ static int _dsim_enable(struct dsim_device *dsim, enum dsim_state state)
 		}
 	}
 
-#if defined(CONFIG_SOC_EXYNOS9810)
 	/* choose OSC_CLK */
 	dsim_reg_set_link_clock(dsim->id, 0);
-#endif
+
 	/* Enable DPHY reset : DPHY reset start */
 	dsim_reg_dphy_resetn(dsim->id, 1);
 
@@ -805,9 +802,9 @@ static int _dsim_enable(struct dsim_device *dsim, enum dsim_state state)
 
 	dsim_reg_set_lanes(dsim->id, dsim->data_lane, 1);
 	dsim_reg_dphy_resetn(dsim->id, 0); /* Release DPHY reset */
-#if defined(CONFIG_SOC_EXYNOS9810)
+
 	dsim_reg_set_link_clock(dsim->id, 1);	/* Selection to word clock */
-#endif
+
 	dsim_reg_set_esc_clk_on_lane(dsim->id, 1, dsim->data_lane);
 	dsim_reg_enable_word_clock(dsim->id, 1);
 
@@ -1064,10 +1061,9 @@ static int dsim_exit_ulps(struct dsim_device *dsim)
 
 	enable_irq(dsim->res.irq);
 
-#if defined(CONFIG_SOC_EXYNOS9810)
 	/* choose OSC_CLK */
 	dsim_reg_set_link_clock(dsim->id, 0);
-#endif
+
 	/* Enable DPHY reset : DPHY reset start */
 	dsim_reg_dphy_resetn(dsim->id, 1);
 	/* DSIM Link SW reset */
@@ -1078,9 +1074,8 @@ static int dsim_exit_ulps(struct dsim_device *dsim)
 
 	dsim_reg_set_lanes(dsim->id, dsim->data_lane, 1);
 	dsim_reg_dphy_resetn(dsim->id, 0); /* release DPHY reset */
-#if defined(CONFIG_SOC_EXYNOS9810)
+
 	dsim_reg_set_link_clock(dsim->id, 1);	/* Selection to word clock */
-#endif
 
 	dsim_reg_set_esc_clk_on_lane(dsim->id, 1, dsim->data_lane);
 	dsim_reg_enable_word_clock(dsim->id, 1);
@@ -1606,7 +1601,6 @@ static int dsim_init_resources(struct dsim_device *dsim, struct platform_device 
 		return -EINVAL;
 	}
 
-#if defined(CONFIG_SOC_EXYNOS9810)
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 1);
 	if (!res) {
 		dsim_err("failed to get mem resource\n");
@@ -1619,7 +1613,6 @@ static int dsim_init_resources(struct dsim_device *dsim, struct platform_device 
 		dsim_err("failed to remap DSIM DPHY SFR region\n");
 		return -EINVAL;
 	}
-#endif
 
 	res = platform_get_resource(pdev, IORESOURCE_IRQ, 0);
 	if (!res) {
@@ -1780,11 +1773,7 @@ static int dsim_runtime_resume(struct device *dev)
 }
 
 static const struct of_device_id dsim_of_match[] = {
-#if defined(CONFIG_SOC_EXYNOS9810)
 	{ .compatible = "samsung,exynos9-dsim" },
-#else
-	{ .compatible = "samsung,exynos8-dsim" },
-#endif
 	{},
 };
 MODULE_DEVICE_TABLE(of, dsim_of_match);
