d0a18489604840104e0c5e6f155a06a311ebc1f0
AlexFurina <92114905+AlexFurina@users.noreply.github.com>
2026-08-22 05:19:58 +0900

    drivers: gpu: arm: exynos: backend: gpexbe_debug_v1: Update to debug-snapshot api   CC      drivers/gpu/arm/bv_r38p1/../exynos/backend/gpexbe_debug_v1.o ../drivers/gpu/arm/bv_r38p1/../exynos/backend/gpexbe_debug_v1.c:21:29: fatal error: linux/exynos-ss.h: No such file or directory  #include <linux/exynos-ss.h>                              ^

--- drivers/gpu/arm/exynos/backend/gpexbe_debug_v1.c
+++ drivers/gpu/arm/exynos/backend/gpexbe_debug_v1.c
@@ -18,12 +18,12 @@
  * http://www.gnu.org/licenses/gpl-2.0.html.
  */
 
-#include <linux/exynos-ss.h>
+#include <linux/debug-snapshot.h>
 #include <gpexbe_debug.h>
 
 void gpexbe_debug_dbg_snapshot_thermal(int frequency)
 {
-	exynos_ss_thermal(NULL, 0, "GPU", frequency);
+	dbg_snapshot_thermal(NULL, 0, "GPU", frequency);
 }
 
 void gpexbe_debug_dbg_snapshot_freq_in(int freq_before, int freq_after)
